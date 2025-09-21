1ab4cae1589eb302c470985fe4d188b12a8f8d01
villas <villas11@gmail.com>
2025-09-21 22:56:11 +0100

    Fix area display to show location instead of N/A

--- script.js
+++ script.js
@@ -71,7 +71,7 @@ class PropertySlideshow {
                         location: property.areaname || 'Location not specified',
                         bedrooms: property.bedrooms || 0,
                         bathrooms: property.bathrooms || 0,
-                        area: 'N/A', // API doesn't provide area data
+                        area: property.areaname || 'Location not specified', // Show location in area field
                         type: property.ptypedescription || 'Property',
                         description: property.description || 'No description available.',
                         images: property.imagegallery,
